63cc6b500292970e94a6a18a62d6ea45e2d65744
Kiderobin <94719017+Kiderobin@users.noreply.github.com>
2024-05-04 01:05:33 -0400

    Add files via upload

---
+++ script/Toon/c1000000003.lua
@@ -0,0 +1,15 @@
+--Toon World Defender
+local s,id=GetID()
+function s.initial_effect(c)
+	--fusion material
+	c:EnableReviveLimit()
+	Fusion.AddProcMix(c,true,true,15270885,38369349)
+    local e2=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetTargetRange(0,LOCATION_MZONE)
+	e1:SetCondition(function(e) return Duel.IsTurnPlayer(1-e:GetHandlerPlayer()) and Duel.IsBattlePhase() end)
+	e1:SetTarget(aux.TargetBoolFunction(Card.IsAttackPos))
+	c:RegisterEffect(e1)
+
+end
