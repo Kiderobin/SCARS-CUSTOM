cc839beb257f8eb473799d3c00f1651d34cc5de4
Kiderobin <94719017+Kiderobin@users.noreply.github.com>
2024-05-04 01:14:03 -0400

    Update c1000000003.lua

--- script/Toon/c1000000003.lua
+++ script/Toon/c1000000003.lua
@@ -4,7 +4,8 @@ function s.initial_effect(c)
 	--fusion material
 	c:EnableReviveLimit()
 	Fusion.AddProcMix(c,true,true,15270885,38369349)
-    local e2=Effect.CreateEffect(c)
+	
+    local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetTargetRange(0,LOCATION_MZONE)
