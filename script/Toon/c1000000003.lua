3f5d9888d78c7842798fc75a5e6f621078fdbc1e
Kiderobin <94719017+Kiderobin@users.noreply.github.com>
2024-05-04 01:25:07 -0400

    Update c1000000003.lua

--- script/Toon/c1000000003.lua
+++ script/Toon/c1000000003.lua
@@ -5,12 +5,12 @@ function s.initial_effect(c)
 	c:EnableReviveLimit()
 	Fusion.AddProcMix(c,true,true,15270885,38369349)
 	
-    local e1=Effect.CreateEffect(c)
+   local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetCode(EFFECT_MUST_ATTACK)
 	e1:SetRange(LOCATION_MZONE)
 	e1:SetTargetRange(0,LOCATION_MZONE)
 	e1:SetCondition(function(e) return Duel.IsTurnPlayer(1-e:GetHandlerPlayer()) and Duel.IsBattlePhase() end)
 	e1:SetTarget(aux.TargetBoolFunction(Card.IsAttackPos))
 	c:RegisterEffect(e1)
-
 end
