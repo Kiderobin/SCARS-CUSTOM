21d6a9028cc08af16554dd74fec62a3507626244
Kiderobin <94719017+Kiderobin@users.noreply.github.com>
2024-05-05 21:58:53 -0400

    Update c1000000004.lua

--- script/Pumpkin/c1000000004.lua
+++ script/Pumpkin/c1000000004.lua
@@ -24,6 +24,6 @@ end
 	e1:SetTargetRange(1,0)
 	e1:SetTarget(s.splimit)
 	e1:SetReset(RESET_PHASE+PHASE_END)
-	Duel.RegisterEffect(e2,tp)
+	Duel.RegisterEffect(e1,tp)
 	aux.RegisterClientHint(e:GetHandler(),nil,tp,1,0,aux.Stringid(id,2),nil)
 end
