e494ca1136a5a820c7b652456f8edd3ea6cf5e48
Kiderobin <94719017+Kiderobin@users.noreply.github.com>
2024-05-05 21:55:55 -0400

    Update c1000000004.lua

--- script/Pumpkin/c1000000004.lua
+++ script/Pumpkin/c1000000004.lua
@@ -12,7 +12,7 @@ c:RegisterEffect(e1)
 
 s.listed_names={96163807}
 function s.spcon(e,c)
-	if c==function then return true end
+	if c==nil then return true end
 	local tp=c:GetControler()
 	return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsExistingMatchingCard(aux.FaceupFilter(Card.IsCode,1000000005),tp,LOCATION_ONFIELD,0,1,nil)
