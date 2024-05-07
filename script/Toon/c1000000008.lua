984aaac1c24f2bc2f578a22263e3c1c19b8da6df
Kiderobin <94719017+Kiderobin@users.noreply.github.com>
2024-05-06 22:56:51 -0400

    Update c1000000008.lua

--- script/Toon/c1000000008.lua
+++ script/Toon/c1000000008.lua
@@ -1,64 +1,84 @@
---Toon King
---Created by ScareTheVoices
-local s,id=GetID()
+-- Toon King
+-- Created by ScareTheVoices
+local s, id = GetID()
+
 function s.initial_effect(c)
-    local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id,0))
+    -- Special Summon effect
+    local e1 = Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id, 0))
     e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+    e1:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
     e1:SetCode(EVENT_SPSUMMON_SUCCESS)
     e1:SetTarget(s.sptg)
     e1:SetOperation(s.spop)
     c:RegisterEffect(e1)
 
-    local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e2:SetOperation(s.atklimit)
-	c:RegisterEffect(e2)
+    -- Attack limit effect
+    local e2 = Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_CONTINUOUS)
+    e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+    e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e2:SetOperation(s.atklimit)
+    c:RegisterEffect(e2)
 
-	--Attack cost
-	local e3=Effect.CreateEffect(c)
-	e3:SetType(EFFECT_TYPE_SINGLE)
-	e3:SetCode(EFFECT_ATTACK_COST)
-	e3:SetCost(s.atcost)
-	e3:SetOperation(s.atop)
-	c:RegisterEffect(e3)
+    -- Attack cost
+    local e3 = Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_SINGLE)
+    e3:SetCode(EFFECT_ATTACK_COST)
+    e3:SetCost(s.atcost)
+    e3:SetOperation(s.atop)
+    c:RegisterEffect(e3)
 end
 
-function s.spfilter(c,e,tp)
-    return c:IsType(TYPE_TOON) and c:IsLevel(4) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+s.listed_names = {43175858}
+
+function s.spfilter(c, e, tp)
+    return c:IsType(TYPE_TOON) and c:IsLevel(4) and c:IsCanBeSpecialSummoned(e, 0, tp, false, false)
 end
 
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>1
-        and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK+LOCATION_HAND,0,2,nil,e,tp) end
-    Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,2,tp,LOCATION_DECK+LOCATION_HAND)
+function s.sptg(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then
+        return Duel.GetLocationCount(tp, LOCATION_MZONE) > 1
+            and Duel.IsExistingMatchingCard(s.spfilter, tp, LOCATION_DECK + LOCATION_HAND, 0, 2, nil, e, tp)
+    end
+    Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, nil, 2, tp, LOCATION_DECK + LOCATION_HAND)
 end
 
-function s.spop(e,tp,eg,ep,ev,re,r,rp)
-    if Duel.GetLocationCount(tp,LOCATION_MZONE)<=1 then return end
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-    local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK+LOCATION_HAND,0,2,2,nil,e,tp)
-    if #g>0 then
-        Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP_DEFENSE)
+function s.spop(e, tp, eg, ep, ev, re, r, rp)
+    if Duel.GetLocationCount(tp, LOCATION_MZONE) <= 1 then return end
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SPSUMMON)
+    local g = Duel.SelectMatchingCard(tp, s.spfilter, tp, LOCATION_DECK + LOCATION_HAND, 0, 2, 2, nil, e, tp)
+    if #g > 0 then
+        Duel.SpecialSummon(g, 0, tp, tp, false, false, POS_FACEUP_DEFENSE)
     end
 end
 
-function s.atklimit(e,tp,eg,ep,ev,re,r,rp)
-	local e1=Effect.CreateEffect(e:GetHandler())
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetCode(EFFECT_CANNOT_ATTACK)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END)
-	e:GetHandler():RegisterEffect(e1)
+function s.atklimit(e, tp, eg, ep, ev, re, r, rp)
+    local fieldSpell = Duel.GetFieldCard(tp, LOCATION_SZONE, 0)
+    if fieldSpell and fieldSpell:IsCode(43175858) then
+        -- Allow direct attacks
+        local e1 = Effect.CreateEffect(e:GetHandler())
+        e1:SetType(EFFECT_TYPE_SINGLE)
+        e1:SetCode(EFFECT_DIRECT_ATTACK)
+        e1:SetReset(RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END)
+        e:GetHandler():RegisterEffect(e1)
+    else
+        -- Apply the existing attack limit effect
+        local e2 = Effect.CreateEffect(e:GetHandler())
+        e2:SetType(EFFECT_TYPE_SINGLE)
+        e2:SetCode(EFFECT_CANNOT_ATTACK)
+        e2:SetReset(RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END)
+        e:GetHandler():RegisterEffect(e2)
+    end
 end
-function s.atcost(e,c,tp)
-	return Duel.CheckLPCost(tp,500)
+
+function s.atcost(e, c, tp)
+    return Duel.CheckLPCost(tp, 500)
 end
-function s.atop(e,tp,eg,ep,ev,re,r,rp)
-	if Duel.IsAttackCostPaid()~=2 and e:GetHandler():IsLocation(LOCATION_MZONE) then
-		Duel.PayLPCost(tp,800)
-		Duel.AttackCostPaid()
-	end
+
+function s.atop(e, tp, eg, ep, ev, re, r, rp)
+    if Duel.IsAttackCostPaid() ~= 2 and e:GetHandler():IsLocation(LOCATION_MZONE) then
+        Duel.PayLPCost(tp, 800)
+        Duel.AttackCostPaid()
+    end
 end
