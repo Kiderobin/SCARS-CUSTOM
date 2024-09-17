6b26be82b2f91f04de4fc50914afe6631cc9184c
Scarecrow <94719017+Kiderobin@users.noreply.github.com>
2024-09-17 19:38:21 -0400

    Add files via upload

---
+++ script/c1000000028.lua
@@ -0,0 +1,41 @@
+-- Black Ritual Seizer Dragon
+local s,id=GetID()
+function s.initial_effect(c)
+    -- XYZ Summon
+    Xyz.AddProcedure(c,nil,8,2)
+    c:EnableReviveLimit()
+    -- Take control of opponent's Ritual monster
+    local e1=Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id,0))
+    e1:SetCategory(CATEGORY_CONTROL)
+    e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetCountLimit(1)
+    e1:SetCondition(s.controlcon)
+    e1:SetCost(s.controlcost)
+    e1:SetTarget(s.controltg)
+    e1:SetOperation(s.controlop)
+    c:RegisterEffect(e1,false,REGISTER_FLAG_DETACH_XMAT)
+end
+
+function s.controlcon(e,tp,eg,ep,ev,re,r,rp)
+    return Duel.IsExistingMatchingCard(Card.IsType,tp,0,LOCATION_MZONE,1,nil,TYPE_RITUAL,TYPE_MONSTER)
+end
+
+function s.controlcost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
+    e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
+end
+
+function s.controltg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsType,tp,0,LOCATION_MZONE,1,nil,TYPE_RITUAL) end
+    Duel.SetOperationInfo(0,CATEGORY_CONTROL,nil,1,0,0)
+end
+
+function s.controlop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONTROL)
+    local g=Duel.SelectMatchingCard(tp,Card.IsType,tp,0,LOCATION_MZONE,1,1,nil,TYPE_RITUAL)
+    if #g>0 then
+        Duel.GetControl(g,tp)
+    end
+end
