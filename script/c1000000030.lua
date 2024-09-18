bf4d5150d71d6e15719c62c300bec2fefa61d477
Scarecrow <94719017+Kiderobin@users.noreply.github.com>
2024-09-18 19:47:44 -0400

    Update c1000000030.lua

--- script/c1000000030.lua
+++ script/c1000000030.lua
@@ -1,26 +1,32 @@
 -- Ritual Summoning Grounds
 local s,id=GetID()
 function s.initial_effect(c)
-    -- Activate effect
+    -- Activate Field Spell
     local e1=Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id,0))
-    e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
-    e1:SetType(EFFECT_TYPE_IGNITION)
-    e1:SetRange(LOCATION_HAND)
-    e1:SetCountLimit(1,id)
-    e1:SetCost(s.cost)
-    e1:SetTarget(s.target)
-    e1:SetOperation(s.operation)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_FREE_CHAIN)
     c:RegisterEffect(e1)
+    
+    -- Manually activate effect with cost
+    local e2=Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id,0))
+    e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetRange(LOCATION_FZONE)
+    e2:SetCountLimit(1,id)
+    e2:SetCost(s.cost)
+    e2:SetTarget(s.target)
+    e2:SetOperation(s.activate)
+    c:RegisterEffect(e2)
 end
 
 function s.cost(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return e:GetHandler():IsDiscardable() end
-    Duel.SendtoGrave(e:GetHandler(),REASON_COST+REASON_DISCARD)
+    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsDiscardable,tp,LOCATION_HAND,0,1,nil) end
+    Duel.DiscardHand(tp,Card.IsDiscardable,1,1,REASON_COST+REASON_DISCARD)
 end
 
 function s.filter(c)
-    return (c:IsType(TYPE_RITUAL) and c:IsAbleToHand()
+    return c:IsType(TYPE_RITUAL) and c:IsAbleToHand()
 end
 
 function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
@@ -28,7 +34,7 @@ function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
     Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
 end
 
-function s.operation(e,tp,eg,ep,ev,re,r,rp)
+function s.activate(e,tp,eg,ep,ev,re,r,rp)
     Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
     local g=Duel.SelectMatchingCard(tp,s.filter,tp,LOCATION_DECK,0,1,1,nil)
     if #g>0 then
