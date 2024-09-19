e743778b9922c19144ebb69357f0880ae070a338
Scarecrow <94719017+Kiderobin@users.noreply.github.com>
2024-09-18 21:06:19 -0400

    Update c1000000031.lua

--- script/c1000000031.lua
+++ script/c1000000031.lua
@@ -1,74 +1,57 @@
 --Djinn Juggler of Rituals
 local s,id=GetID()
 function s.initial_effect(c)
-	--gain
-	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
-	e1:SetProperty(EFFECT_FLAG_EVENT_PLAYER+EFFECT_FLAG_CANNOT_DISABLE)
-	e1:SetCode(EVENT_BE_MATERIAL)
-	e1:SetCountLimit(1,id)
-	e1:SetCondition(s.mtcon)
-	e1:SetOperation(s.mtop)
-	c:RegisterEffect(e1)
-	--banish for ritual
-	local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_IGNITION)
-	e2:SetRange(LOCATION_GRAVE)
-	e2:SetCountLimit(1,{id,1})
-	e2:SetCondition(aux.exccon)
-	e2:SetTarget(s.rtg)
-	e2:SetOperation(s.rop)
-	c:RegisterEffect(e2)
+    --Can be used for a Ritual Summon while it is in the GY
+    local e1=Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e1:SetCode(EFFECT_EXTRA_RITUAL_MATERIAL)
+    e1:SetRange(LOCATION_GRAVE)
+    e1:SetCondition(s.con)
+    e1:SetValue(1)
+    c:RegisterEffect(e1)
+    --Grants an effect if it is used for a Ritual Summon
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_CONTINUOUS)
+    e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+    e2:SetCode(EVENT_BE_MATERIAL)
+    e2:SetCondition(s.condition)
+    e2:SetOperation(s.operation)
+    c:RegisterEffect(e2)
 end
-s.listed_series={0x10cf}
-function s.mtcon(e,tp,eg,ep,ev,re,r,rp)
-	return r==REASON_RITUAL and eg:IsExists(Card.IsType,1,nil,TYPE_RITUAL)
+
+function s.con(e)
+    return true
 end
-function s.mtop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	local g=eg:Filter(Card.IsType,nil,TYPE_RITUAL)
-	local rc=g:GetFirst()
-	if not rc then return end
-	local e1=Effect.CreateEffect(rc)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_TODECK)
-	e1:SetType(EFFECT_TYPE_IGNITION)
-	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e1:SetRange(LOCATION_MZONE)
-	e1:SetCountLimit(1)
-	e1:SetTarget(s.tdtg)
-	e1:SetOperation(s.tdop)
-	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
-	rc:RegisterEffect(e1,true)
-	if not rc:IsType(TYPE_EFFECT) then
-		local e2=Effect.CreateEffect(c)
-		e2:SetType(EFFECT_TYPE_SINGLE)
-		e2:SetCode(EFFECT_ADD_TYPE)
-		e2:SetValue(TYPE_EFFECT)
-		e2:SetReset(RESET_EVENT+RESETS_STANDARD)
-		rc:RegisterEffect(e2,true)
-	end
-	rc:RegisterFlagEffect(0,RESET_EVENT+RESETS_STANDARD,EFFECT_FLAG_CLIENT_HINT,1,0,aux.Stringid(id,3))
+function s.condition(e,tp,eg,ep,ev,re,r,rp)
+    return r==REASON_RITUAL
 end
-function s.tdtg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_REMOVED) and chkc:IsAbleToDeck() end
-	if chk==0 then return Duel.IsExistingTarget(Card.IsAbleToDeck,tp,LOCATION_REMOVED,0,1,nil) end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-	local g=Duel.SelectTarget(tp,Card.IsAbleToDeck,tp,LOCATION_REMOVED,0,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_TODECK,g,1,0,0)
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+    for rc in eg:Iter() do
+        if rc:GetFlagEffect(id)==0 then
+            --Return 1 banished monster to the deck as an ignition effect
+            local e1=Effect.CreateEffect(e:GetHandler())
+            e1:SetDescription(aux.Stringid(id,0))
+            e1:SetCategory(CATEGORY_TODECK)
+            e1:SetType(EFFECT_TYPE_IGNITION)
+            e1:SetRange(LOCATION_MZONE)
+            e1:SetCountLimit(1)
+            e1:SetTarget(s.target)
+            e1:SetOperation(s.returnop)
+            e1:SetReset(RESET_EVENT|RESETS_STANDARD)
+            rc:RegisterEffect(e1,true)
+            rc:RegisterFlagEffect(id,RESET_EVENT|RESETS_STANDARD,0,1)
+        end
+    end
 end
-function s.tdop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) then
-		Duel.SendtoDeck(tc,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
-	end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsAbleToDeck,tp,LOCATION_REMOVED,0,1,nil) end
+    Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,0,LOCATION_REMOVED)
 end
-function s.rtg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(aux.RitualUltimateFilter,tp,LOCATION_HAND+LOCATION_GRAVE,0,1,nil,e,tp,aux.FilterBoolFunction(Card.IsType,TYPE_RITUAL),nil,Card.GetLevel,"Equal") end
-	Duel.SetOperationInfo(0,CATEGORY_REMOVE,e:GetHandler(),1,0,0)
-end
-function s.rop(e,tp,eg,ep,ev,re,r,rp)
-	local c=e:GetHandler()
-	if not c:IsRelateToEffect(e) then return end
-	Duel.Remove(c,POS_FACEUP,REASON_EFFECT)
+function s.returnop(e,tp,eg,ep,ev,re,r,rp)
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+    local g=Duel.SelectMatchingCard(tp,Card.IsAbleToDeck,tp,LOCATION_REMOVED,0,1,1,nil)
+    if #g>0 then
+        Duel.SendtoDeck(g,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+    end
 end
