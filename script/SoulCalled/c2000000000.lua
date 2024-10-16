c5d7a48de76da513190ec03edd5e62328eed740a
Scarecrow <94719017+Kiderobin@users.noreply.github.com>
2024-10-16 14:27:52 -0400

    Update c2000000000.lua

--- script/SoulCalled/c2000000000.lua
+++ script/SoulCalled/c2000000000.lua
@@ -1,92 +1,92 @@
--- Custom Yu-Gi-Oh! Monster for Project Ignis
+-- Soulcalled, Gravedigger
 local s, id = GetID()
 function s.initial_effect(c)
-    -- Cannot be Normal Summoned
-    local e1=Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
-    e1:SetCode(EFFECT_CANNOT_SUMMON)
-    c:RegisterEffect(e1)
-
-    -- Banish opponent's monster in battle
-    local e2=Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id,0))
-    e2:SetCategory(CATEGORY_REMOVE)
-    e2:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_F)
-    e2:SetCode(EVENT_BATTLE_START)
-    e2:SetTarget(s.bantg)
-    e2:SetOperation(s.banop)
-    c:RegisterEffect(e2)
-
-    -- Add Soul Counters
-    local e3=Effect.CreateEffect(c)
-    e3:SetType(EFFECT_TYPE_CONTINUOUS + EFFECT_TYPE_FIELD)
-    e3:SetCode(EVENT_REMOVE)
-    e3:SetRange(LOCATION_MZONE)
-    e3:SetCondition(s.ctcon)
-    e3:SetOperation(s.ctop)
-    c:RegisterEffect(e3)
-
-    -- Register Soul Counter
-    c:EnableCounterPermit(0x239b)
-    c:SetCounterLimit(0x239b,12)
-
-    -- Add card to hand when destroyed or banished
-    local e4=Effect.CreateEffect(c)
-    e4:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
-    e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP + EFFECT_FLAG_DELAY)
-    e4:SetCode(EVENT_TO_GRAVE)
-    e4:SetCondition(s.retcon)
-    e4:SetTarget(s.rettg)
-    e4:SetOperation(s.retop)
-    c:RegisterEffect(e4)
-    local e5=e4:Clone()
-    e5:SetCode(EVENT_REMOVE)
-    c:RegisterEffect(e5)
+   -- Cannot be Normal Summoned
+   local e1=Effect.CreateEffect(c)
+   e1:SetType(EFFECT_TYPE_SINGLE)
+   e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
+   e1:SetCode(EFFECT_CANNOT_SUMMON)
+   c:RegisterEffect(e1)
+   -- Banish opponent's monster in battle
+   local e2=Effect.CreateEffect(c)
+   e2:SetDescription(aux.Stringid(id,0))
+   e2:SetCategory(CATEGORY_REMOVE)
+   e2:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_F)
+   e2:SetCode(EVENT_BATTLE_START)
+   e2:SetTarget(s.bantg)
+   e2:SetOperation(s.banop)
+   c:RegisterEffect(e2)
+   -- Add Soul Counters
+   local e3=Effect.CreateEffect(c)
+   e3:SetType(EFFECT_TYPE_CONTINUOUS + EFFECT_TYPE_FIELD)
+   e3:SetCode(EVENT_REMOVE)
+   e3:SetRange(LOCATION_MZONE)
+   e3:SetCondition(s.ctcon)
+   e3:SetOperation(s.ctop)
+   c:RegisterEffect(e3)
+   -- Register Soul Counter
+   c:EnableCounterPermit(0x239b)
+   c:SetCounterLimit(0x239b,12)
+   -- Add card to hand when destroyed or banished
+   local e4=Effect.CreateEffect(c)
+   e4:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+   e4:SetProperty(EFFECT_FLAG_DAMAGE_STEP + EFFECT_FLAG_DELAY)
+   e4:SetCode(EVENT_TO_GRAVE)
+   e4:SetCondition(s.retcon)
+   e4:SetTarget(s.rettg)
+   e4:SetOperation(s.retop)
+   c:RegisterEffect(e4)
+   local e5=e4:Clone()
+   e5:SetCode(EVENT_REMOVE)
+   c:RegisterEffect(e5)
 end
 
 function s.bantg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return true end
-    local c=e:GetHandler()
-    local bc=c:GetBattleTarget()
-    if bc and bc:IsFaceup() and bc:IsAttackAbove(c:GetAttack()) then
-        Duel.SetOperationInfo(0,CATEGORY_REMOVE,bc,1,0,0)
-    end
+   if chk==0 then return true end
+   local c=e:GetHandler()
+   local bc=c:GetBattleTarget()
+   if bc and bc:IsFaceup() and bc:IsAttackAbove(c:GetAttack()) then
+       Duel.SetOperationInfo(0,CATEGORY_REMOVE,bc,1,0,0)
+   end
 end
 
 function s.banop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    local bc=c:GetBattleTarget()
-    if bc and bc:IsRelateToBattle() and bc:IsFaceup() and bc:IsAttackAbove(c:GetAttack()) then
-        Duel.Remove(bc,POS_FACEUP,REASON_EFFECT)
-    end
+   local c=e:GetHandler()
+   local bc=c:GetBattleTarget()
+   if bc and bc:IsRelateToBattle() and bc:IsFaceup() and bc:IsAttackAbove(c:GetAttack()) then
+       Duel.Remove(bc,POS_FACEUP,REASON_EFFECT)
+       -- Add Soul Counter when a monster is banished by this card's effect
+       if c:IsRelateToEffect(e) then
+           c:AddCounter(0x239b,1)
+       end
+   end
 end
 
 function s.ctcon(e,tp,eg,ep,ev,re,r,rp)
-    return eg:IsExists(Card.IsControler,1,nil,1-tp) and eg:IsExists(Card.IsReason,1,nil,REASON_EFFECT)
+   return eg:IsExists(Card.IsControler,1,nil,1-tp) and eg:IsExists(Card.IsReason,1,nil,REASON_EFFECT)
 end
 
 function s.ctop(e,tp,eg,ep,ev,re,r,rp)
-    local c=e:GetHandler()
-    if c:IsRelateToEffect(e) then
-        c:AddCounter(0x239b,1)
-    end
+   local c=e:GetHandler()
+   if c:IsRelateToEffect(e) then
+       c:AddCounter(0x239b,1)
+   end
 end
 
 function s.retcon(e,tp,eg,ep,ev,re,r,rp)
-    return e:GetHandler():IsReason(REASON_DESTROY)
+   return e:GetHandler():IsReason(REASON)
 end
 
 function s.rettg(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_GRAVE + LOCATION_DECK,0,1,nil,2000000001) end
-    Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE + LOCATION_DECK)
+   if chk==0 then return Duel.IsExistingMatchingCard(Card.IsCode,tp,LOCATION_GRAVE + LOCATION_DECK,0,1,nil,2000000001) end
+   Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE + LOCATION_DECK)
 end
 
 function s.retop(e,tp,eg,ep,ev,re,r,rp)
-    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-    local g=Duel.SelectMatchingCard(tp,Card.IsCode,tp,LOCATION_GRAVE + LOCATION_DECK,0,1,1,nil,2000000001)
-    if #g > 0 then
-        Duel.SendtoHand(g,nil,REASON_EFFECT)
-        Duel.ConfirmCards(1-tp,g)
-    end
+   Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+   local g=Duel.SelectMatchingCard(tp,Card.IsCode,tp,LOCATION_GRAVE + LOCATION_DECK,0,1,1,nil,2000000001)
+   if #g > 0 then
+       Duel.SendtoHand(g,nil,REASON_EFFECT)
+       Duel.ConfirmCards(1-tp,g)
+   end
 end
