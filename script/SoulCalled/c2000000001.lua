f2a206c8b3ac8e60435eaf469ed383f182c87e64
Scarecrow <94719017+Kiderobin@users.noreply.github.com>
2024-10-16 14:07:11 -0400

    Update c2000000001.lua

--- script/SoulCalled/c2000000001.lua
+++ script/SoulCalled/c2000000001.lua
@@ -1,47 +1,99 @@
--- Soul Call
-function c2000000001.initial_effect(c)
-    -- Equip procedure
-    aux.AddEquipProcedure(c, nil, aux.FilterBoolFunction(Card.IsControler, tp))
-
-    -- Special Summon
+--Soul Call
+local s,id=GetID()
+function s.initial_effect(c)
+    --Activate
     local e1=Effect.CreateEffect(c)
-    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e1:SetType(EFFECT_TYPE_IGNITION)
-    e1:SetRange(LOCATION_SZONE)
-    e1:SetTarget(c2000000001.target)
-    e1:SetOperation(c2000000001.activate)
+    e1:SetCategory(CATEGORY_EQUIP)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e1:SetTarget(s.target)
+    e1:SetOperation(s.operation)
     c:RegisterEffect(e1)
+    --Equip limit
+    local e2=Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetCode(EFFECT_EQUIP_LIMIT)
+    e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+    e2:SetValue(s.eqlimit)
+    c:RegisterEffect(e2)
+    --Change attribute to DARK
+    local e3=Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_EQUIP)
+    e3:SetCode(EFFECT_CHANGE_ATTRIBUTE)
+    e3:SetValue(ATTRIBUTE_DARK)
+    c:RegisterEffect(e3)
+    --Special Summon from deck, hand, or grave
+    local e4=Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id,0))
+    e4:SetCategory(CATEGORY_SPECIAL_SUMMON+CATEGORY_RELEASE)
+    e4:SetType(EFFECT_TYPE_IGNITION)
+    e4:SetRange(LOCATION_SZONE)
+    e4:SetCountLimit(1)
+    e4:SetTarget(s.sptg)
+    e4:SetOperation(s.spop)
+    c:RegisterEffect(e4)
+    --Change original ATK/DEF to tributed monster's
+    local e5=Effect.CreateEffect(c)
+    e5:SetType(EFFECT_TYPE_SINGLE)
+    e5:SetCode(EFFECT_SET_BASE_ATTACK)
+    e5:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+    e5:SetValue(s.atkval)
+    c:RegisterEffect(e5)
+    local e6=e5:Clone()
+    e6:SetCode(EFFECT_SET_BASE_DEFENSE)
+    e6:SetValue(s.defval)
+    c:RegisterEffect(e6)
 end
 
-function c2000000001.filter(c,e,tp)
-    return c:IsCanBeSpecialSummoned(e,0,tp,false,false) and c:IsCode(2000000000)
+function s.eqlimit(e,c)
+    return c:IsControler(e:GetHandlerPlayer())
 end
 
-function c2000000001.target(e,tp,eg,ep,ev,re,r,rp,chk)
-    if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-        and Duel.IsExistingMatchingCard(c2000000001.filter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,nil,e,tp) end
+function s.target(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+    if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_MZONE) end
+    if chk==0 then return Duel.IsExistingTarget(aux.TRUE,tp,LOCATION_MZONE,0,1,nil) end
+    Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_EQUIP)
+    Duel.SelectTarget(tp,aux.TRUE,tp,LOCATION_MZONE,0,1,1,nil)
+end
+
+function s.operation(e,tp,eg,ep,ev,re,r,rp)
+    local c=e:GetHandler()
+    local tc=Duel.GetFirstTarget()
+    if c:IsRelateToEffect(e) and tc:IsRelateToEffect(e) and tc:IsFaceup() then
+        Duel.Equip(tp,c,tc)
+    end
+end
+
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+    local c=e:GetHandler()
+    local tc=c:GetEquipTarget()
+    if chk==0 then return tc and Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+        and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,nil,e,tp) end
+    Duel.SetOperationInfo(0,CATEGORY_RELEASE,tc,1,0,0)
     Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE)
 end
 
-function c2000000001.activate(e,tp,eg,ep,ev,re,r,rp)
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
     local c=e:GetHandler()
     local tc=c:GetEquipTarget()
-    if tc and Duel.SendtoGrave(tc,REASON_COST)~=0 then
-        if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
+    if tc and Duel.Release(tc,REASON_EFFECT)~=0 and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 then
         Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-        local g=Duel.SelectMatchingCard(tp,c2000000001.filter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,e,tp)
-        if g:GetCount()>0 then
-            local sc=g:GetFirst()
+        local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_HAND+LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil,e,tp)
+        local sc=g:GetFirst()
+        if sc then
             Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP)
             local atk=tc:GetBaseAttack()
             local def=tc:GetBaseDefense()
-            if atk>=0 and def>=0 then
+            if atk>0 then
                 local e1=Effect.CreateEffect(c)
                 e1:SetType(EFFECT_TYPE_SINGLE)
                 e1:SetCode(EFFECT_SET_BASE_ATTACK)
                 e1:SetValue(atk)
                 e1:SetReset(RESET_EVENT+RESETS_STANDARD)
                 sc:RegisterEffect(e1)
+            end
+            if def>0 then
                 local e2=Effect.CreateEffect(c)
                 e2:SetType(EFFECT_TYPE_SINGLE)
                 e2:SetCode(EFFECT_SET_BASE_DEFENSE)
@@ -51,4 +103,16 @@ function c2000000001.activate(e,tp,eg,ep,ev,re,r,rp)
             end
         end
     end
+end
+
+function s.spfilter(c,e,tp)
+    return c:IsSetCard(0x1317) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+
+function s.atkval(e,c)
+    return e:GetHandler():GetEquipTarget():GetBaseAttack()
+end
+
+function s.defval(e,c)
+    return e:GetHandler():GetEquipTarget():GetBaseDefense()
 end
