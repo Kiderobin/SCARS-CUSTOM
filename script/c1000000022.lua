59e58b084f52b411d1ab8cb254733cae9eb4a09e
Scarecrow <94719017+Kiderobin@users.noreply.github.com>
2025-03-25 18:18:38 -0400

    Update c1000000022.lua

--- script/c1000000022.lua
+++ script/c1000000022.lua
@@ -5,6 +5,8 @@ function s.initial_effect(c)
 	--Xyz summon procedure
 	Xyz.AddProcedure(c,nil,4,2)
 	c:EnableReviveLimit()
+	
+	-- Search and add Spellcaster monster to hand
 	local e1=Effect.CreateEffect(c)
 	e1:SetDescription(aux.Stringid(id,0))
 	e1:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
@@ -15,33 +17,27 @@ function s.initial_effect(c)
 	e1:SetTarget(s.sptg)
 	e1:SetOperation(s.spop)
 	c:RegisterEffect(e1,false,REGISTER_FLAG_DETACH_XMAT)
-
-    local e2=Effect.CreateEffect(c)
-	e2:SetType(EFFECT_TYPE_SINGLE)
-	e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e2:SetRange(LOCATION_MZONE)
-	e2:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
-	e2:SetCountLimit(1)
-	e2:SetValue(s.valcon)
-	c:RegisterEffect(e2)
 end
 
-function s.valcon(e,re,r,rp)
-	return (r&REASON_BATTLE+r&REASON_EFFECT)~=0
-end
-
+-- Cost: Detach 1 Xyz Material
 function s.spcost(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return e:GetHandler():CheckRemoveOverlayCard(tp,1,REASON_COST) end
 	e:GetHandler():RemoveOverlayCard(tp,1,1,REASON_COST)
 end
+
+-- Filter for Spellcaster-type monsters
 function s.spfilter(c,e,tp)
 	return c:IsRace(RACE_SPELLCASTER) and c:IsAbleToHand()
 end
+
+-- Target: Search for a Spellcaster monster from Graveyard or Deck
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
 		and Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp) end
 	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_GRAVE+LOCATION_DECK)
 end
+
+-- Operation: Add the selected Spellcaster monster to hand
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
