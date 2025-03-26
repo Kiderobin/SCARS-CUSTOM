9722936f3cb439d60b8ff6daabc161763406d077
Scarecrow <94719017+Kiderobin@users.noreply.github.com>
2025-03-26 19:50:14 -0400

    Update c1000000015.lua

--- script/Ritual Dragon/c1000000015.lua
+++ script/Ritual Dragon/c1000000015.lua
@@ -1,38 +1,32 @@
---Emerald Eyes
+--Emerald Lights
 --Created by ScareTheVoices
 local s,id=GetID()
 function s.initial_effect(c)
-	aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop)
+    aux.AddSkillProcedure(c,1,false,s.flipcon,s.flipop)
 end
 function s.flipcon(e,tp,eg,ep,ev,re,r,rp)
-	--condition
-	return aux.CanActivateSkill(tp) and Duel.IsExistingMatchingCard(s.cffilter,tp,LOCATION_HAND,0,1,nil)
-	and Duel.GetFlagEffect(tp,id)==0
+    --condition
+    return aux.CanActivateSkill(tp) and Duel.IsExistingMatchingCard(s.cffilter,tp,LOCATION_HAND,0,1,nil)
+    and Duel.GetFlagEffect(tp,id)==0
 end
 function s.cffilter(c)
-	return c:IsExistingMatchingCard(1000000000)
-end
-function s.ritfilter(c,tc)
-	return c:IsRitualSpell() and Ritual.GetMatchingFilterFunction(c)(tc)
+    return c:IsCode(1000000000) -- Checks for "Emerald Sovereign Ritual Dragon"
 end
 function s.flipop(e,tp,eg,ep,ev,re,r,rp)
-	Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
-	Duel.Hint(HINT_CARD,tp,id)
-	--opd register
-	Duel.RegisterFlagEffect(tp,id,0,0,0)
-	local g=Duel.SelectMatchingCard(tp,s.cffilter,tp,LOCATION_HAND,0,1,1,e:GetHandler())
-	Duel.ConfirmCards(1-tp,g)
-	local tc=g:GetFirst()
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local sg=Duel.SelectMatchingCard(tp,s.ritfilter,tp,LOCATION_DECK,0,1,1,e:GetHandler(),tc)
-	if #sg>0 then
-		Duel.SendtoHand(sg,nil,REASON_EFFECT)
-		Duel.ConfirmCards(1-tp,sg)
-		Duel.ShuffleDeck(tp)
-		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
-		local rg=Duel.SelectMatchingCard(tp,Card.IsAbleToDeck,tp,LOCATION_HAND,0,1,1,nil)
-		if #rg>0 then
-			Duel.SendtoDeck(rg,tp,SEQ_DECKBOTTOM,REASON_EFFECT)
-		end
-	end
+    Duel.Hint(HINT_SKILL_FLIP,tp,id|(1<<32))
+    Duel.Hint(HINT_CARD,tp,id)
+    -- opd register
+    Duel.RegisterFlagEffect(tp,id,0,0,0)
+    -- Select and reveal "Emerald Sovereign Ritual Dragon"
+    local g=Duel.SelectMatchingCard(tp,s.cffilter,tp,LOCATION_HAND,0,1,1,nil)
+    Duel.ConfirmCards(1-tp,g)
+    local tc=g:GetFirst()
+    if tc then
+        -- Add the Ritual Spell with ID 1000000001 from outside the deck
+        local spell=Duel.CreateToken(tp,1000000001)
+        if spell then
+            Duel.SendtoHand(spell,nil,REASON_RULE)
+            Duel.ConfirmCards(1-tp,spell)
+        end
+    end
 end
