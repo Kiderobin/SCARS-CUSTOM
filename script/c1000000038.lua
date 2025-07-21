7a5f1cc121e6b43e8d864dc58d22a8c14213f0c2
Scarecrow <94719017+Kiderobin@users.noreply.github.com>
2025-07-21 06:31:45 -0400

    Add files via upload

---
+++ script/c1000000038.lua
@@ -0,0 +1,50 @@
+--Emerald Light Gateway
+local s,id=GetID()
+function s.initial_effect(c)
+	--Activate
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
+	c:RegisterEffect(e1)
+	--When a setcode 0x4003 monster is Special Summoned, add 1 Emerald Sovereign Ritual Dragon from Deck or Banished Zone to hand
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetProperty(EFFECT_FLAG_DELAY)
+	e2:SetRange(LOCATION_FZONE)
+	e2:SetCountLimit(1,id)
+	e2:SetCondition(s.thcon)
+	e2:SetTarget(s.thtg)
+	e2:SetOperation(s.thop)
+	c:RegisterEffect(e2)
+end
+s.listed_series={0x4003}
+s.listed_names={1000000000}
+
+--Condition for search effect (0x4003 monster Special Summoned)
+function s.thcon(e,tp,eg,ep,ev,re,r,rp)
+	return eg:IsExists(function(c) return c:IsSetCard(0x4003) and c:IsMonster() end,1,nil)
+end
+
+--Filter for monsters to add to hand
+function s.thfilter(c)
+	return c:IsCode(1000000000) and c:IsAbleToHand()
+end
+
+--Target for search effect
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_DECK+LOCATION_REMOVED,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_REMOVED)
+end
+
+--Operation for search effect
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_DECK+LOCATION_REMOVED,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
+end
