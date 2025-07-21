b8cf354991e24d9389eeb12a26b7222f28798c36
Scarecrow <94719017+Kiderobin@users.noreply.github.com>
2025-07-21 03:01:06 -0400

    Update c1000000035.lua

--- script/c1000000035.lua
+++ script/c1000000035.lua
@@ -1,52 +1,65 @@
 --Emerald Light Protection
 local s,id=GetID()
 function s.initial_effect(c)
-	--Activate as a Continuous Trap
-	c:EnableReviveLimit()
-	--Protect Emerald Sovereign Ritual Dragon and Emerald Light monsters
+	--Set as Continuous Trap
+	c:SetUniqueOnField(1,0,id)
+	--Activate (flip face-up)
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_DESTROY)
-	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_O)
-	e1:SetCode(EVENT_DESTROYED)
-	e1:SetRange(LOCATION_SZONE)
-	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
-	e1:SetCountLimit(1,id)
-	e1:SetCondition(s.reptg)
-	e1:SetTarget(s.reptg)
-	e1:SetOperation(s.repop)
+	e1:SetType(EFFECT_TYPE_ACTIVATE)
+	e1:SetCode(EVENT_FREE_CHAIN)
 	c:RegisterEffect(e1)
+	--Protect one chosen Emerald Sovereign Ritual Dragon or Emerald Light monster
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_DESTROY)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EFFECT_DESTROY_REPLACE)
+	e2:SetRange(LOCATION_SZONE)
+	e2:SetCountLimit(1,id)
+	e2:SetTarget(s.reptg)
+	e2:SetValue(s.repval)
+	e2:SetOperation(s.repop)
+	c:RegisterEffect(e2)
 end
 s.listed_series={0x4003}
 s.listed_names={1000000000}
 
---Filter for protected monsters
+--Filter for monsters to protect (ID 1000000000 or setcode 0x4003)
 function s.repfilter(c,tp)
-	return c:IsPreviousLocation(LOCATION_ONFIELD) and c:IsPreviousPosition(POS_FACEUP)
-		and (c:IsCode(1000000000) or c:IsSetCard(0x4003)) and c:IsReason(REASON_EFFECT)
-		and c:GetPreviousControler()==tp
+	return c:IsFaceup() and (c:IsCode(1000000000) or c:IsSetCard(0x4003))
+		and c:IsControler(tp) and c:IsLocation(LOCATION_MZONE) and c:IsReason(REASON_EFFECT)
+		and not c:IsReason(REASON_REPLACE)
 end
 
 --Filter for monsters to destroy instead
-function s.desfilter(c)
-	return c:IsFaceup() and c:IsSetCard(0x4003) and c:IsDestructable()
+function s.desfilter(c,protected)
+	return c:IsFaceup() and c:IsSetCard(0x4003) and c:IsDestructable() and not protected:IsContains(c)
 end
 
---Condition and target for replacement effect
+--Target for replacement effect
 function s.reptg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return eg:IsExists(s.repfilter,1,nil,tp)
-		and Duel.IsExistingMatchingCard(s.desfilter,tp,LOCATION_MZONE,0,1,nil) end
-	if not Duel.SelectYesNo(tp,aux.Stringid(id,0)) then return false end
-	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
-	local g=Duel.SelectTarget(tp,s.desfilter,tp,LOCATION_MZONE,0,1,1,nil)
-	Duel.SetOperationInfo(0,CATEGORY_DESTROY,g,1,0,0)
+	if chk==0 then return e:GetHandler():IsFaceup() and eg:IsExists(s.repfilter,1,nil,tp)
+		and Duel.IsExistingMatchingCard(s.desfilter,tp,LOCATION_MZONE,0,1,nil,Group.CreateGroup()) end
+	if not Duel.SelectEffectYesNo(tp,e:GetHandler(),aux.Stringid(id,0)) then return false end
+	Duel.Hint(HINT_SELECTMSG,tp,aux.Stringid(id,1))
+	local g=eg:FilterSelect(tp,s.repfilter,1,1,nil,tp)
+	if #g==0 then return false end
+	e:SetLabelObject(g)
 	return true
+end
+
+--Value for replacement effect (protect only the chosen monster)
+function s.repval(e,c)
+	return e:GetLabelObject():IsContains(c)
 end
 
 --Operation to destroy a card instead
 function s.repop(e,tp,eg,ep,ev,re,r,rp)
-	local tc=Duel.GetFirstTarget()
-	if tc:IsRelateToEffect(e) and tc:IsFaceup() then
-		Duel.Destroy(tc,REASON_EFFECT+REASON_REPLACE)
+	local protected=e:GetLabelObject()
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_DESTROY)
+	local g=Duel.SelectMatchingCard(tp,s.desfilter,tp,LOCATION_MZONE,0,1,1,nil,protected)
+	if #g>0 then
+		Duel.HintSelection(g)
+		Duel.Destroy(g,REASON_EFFECT+REASON_REPLACE)
 	end
 end
