ba49b1ef23c6ede56f535605ffbcdf91eebd8526
Scarecrow <94719017+Kiderobin@users.noreply.github.com>
2025-07-23 21:27:51 -0400

    Update c1000000039.lua

--- script/c1000000039.lua
+++ script/c1000000039.lua
@@ -1,7 +1,6 @@
---Emerald Light, Shine Bright!
 local s,id=GetID()
 function s.initial_effect(c)
-	--Special Summon 1 "Emerald Light" monster from hand or Deck, then optionally from GY
+	--Special Summon 1 "Emerald Light" monster from hand or Deck
 	local e1=Effect.CreateEffect(c)
 	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
 	e1:SetType(EFFECT_TYPE_ACTIVATE)
@@ -23,17 +22,12 @@ function s.spfilter1(c,e,tp)
 	return c:IsSetCard(0x4003) and c:IsMonster() and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 
---Filter for Special Summon from Graveyard
-function s.spfilter2(c,e,tp)
-	return c:IsSetCard(0x4003) and c:IsMonster() and c:IsCanBeSpecialSummoned(e,0,tp,false,false,POS_FACEUP)
-end
-
 --Target for Special Summon effect
 function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
 	if chk==0 then return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-		and Duel.IsExistingMatchingCard(s.spfilter1,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp) end
+		and Duel.IsExistingMatchingCard(s.spfilter1,tp,LOCATION_HAND+LOCATION_DECK,0,1,nil,e,tp)
+		and Duel.GetFieldGroupCount(tp,LOCATION_MZONE,0)==0 end
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_HAND+LOCATION_DECK)
-	Duel.SetPossibleOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,1,tp,LOCATION_GRAVE)
 end
 
 --Operation for Special Summon effect
@@ -52,17 +46,6 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g1=Duel.SelectMatchingCard(tp,s.spfilter1,tp,LOCATION_HAND+LOCATION_DECK,0,1,1,nil,e,tp)
 	if #g1>0 then
-		if Duel.SpecialSummon(g1,0,tp,tp,false,false,POS_FACEUP)>0 then
-			--Optional Special Summon from Graveyard
-			if Duel.GetLocationCount(tp,LOCATION_MZONE)>0
-				and Duel.IsExistingMatchingCard(s.spfilter2,tp,LOCATION_GRAVE,0,1,nil,e,tp)
-				and Duel.SelectYesNo(tp,0) then
-				Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
-				local g2=Duel.SelectMatchingCard(tp,s.spfilter2,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
-				if #g2>0 then
-					Duel.SpecialSummon(g2,0,tp,tp,false,false,POS_FACEUP)
-				end
-			end
-		end
+		Duel.SpecialSummon(g1,0,tp,tp,false,false,POS_FACEUP)
 	end
 end
